619f2090dc500be180d2f4c7e7363641967e11f0
Volkan Sönmez <volkan.sonmez@etts.com.tr>
2019-12-24 10:34:26 +0300

    Initial Commit

---
+++ UIStoryView/UIStoryView.h
@@ -0,0 +1,19 @@
+//
+//  UIStoryView.h
+//  UIStoryView
+//
+//  Created by Volkan Sonmez on 24.12.2019.
+//  Copyright © 2019 Porte. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIStoryView.
+FOUNDATION_EXPORT double UIStoryViewVersionNumber;
+
+//! Project version string for UIStoryView.
+FOUNDATION_EXPORT const unsigned char UIStoryViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIStoryView/PublicHeader.h>
+
+
